08e3e9ef8f36443ef5712a46aaf04abdf75f04e4
stefania masu <stefania.masu@gmail.com>
2025-03-24 20:36:04 +0100

    copied code fom git

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_24_182520) do
 
   create_table "apps", force: :cascade do |t|
     t.string "name"
-    t.string "developer"
-    t.string "icon_url"
-    t.string "platform"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "apple_id"
